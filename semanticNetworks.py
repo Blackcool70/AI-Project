4b9a567e1eb63fefeab99fd2b33ce0b580470c56
Atef Yassine <32244390+Blackcool70@users.noreply.github.com>
2019-10-29 19:19:46 -0500

    removed toStr
    Removed toStr from this file since it has it's own file.

--- semanticNetworks.py
+++ semanticNetworks.py
@@ -1,21 +1,4 @@
-#	converts things to strings -- even tuples, lists, and dicts
-def tostr(x):
-	t = type(x)
-	if t == dict:
-		return '{' + ", ".join( \
-			list(map( lambda k,d=x: tostr(k)+": "+tostr(d[k]), \
-			list(x.keys()) ))) + "}"
-		
-
-	if t == list:
-		return '[' + ", ".join( \
-			[tostr(i) for i in x], \
-			) + "]"
-
-	return str(x)
-
 # Semantic Network stuff
-
 class Entity:
 	def __init__(self,id):
 		self.id = id
